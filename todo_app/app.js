bddc2c867c42767491e98ffc3693a493d8659233
Yuki-tea <yuuki4168@gmail.com>
2025-04-15 00:11:23 +0900

    add priority in Japanese

--- todo_app/app.js
+++ todo_app/app.js
@@ -1,14 +1,18 @@
 const taskInput = document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
+const priorityInput = document.getElementById("priorityInput");
+
 
 addBtn.addEventListener("click", ()=>{
+    const priority = priorityInput.value; 
     const taskText = taskInput.value.trim();
     if(taskText === "") return;
 
     const li = document.createElement("li");
     li.innerHTML = `
         ${taskText}
+        <span class="priority ${priority}">[${priority}]</span>
         <button class = "deleteBtn">削除</button>
     `;
     
